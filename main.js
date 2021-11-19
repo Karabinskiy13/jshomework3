477c0c16ff322606a093b3e7c274b88a49401205
Karabinskiy13 <ivan.karabinskiy1308@gmail.com>
2021-11-19 12:37:07 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,100 @@
+// let num=[1, 2, 3, 4, 5];
+// let string=['html', 'js', 'react', 'node', 'vue'];
+// let mix=[1,'html', true, false, 123]
+// console.log(num);
+// console.log(string);
+// console.log(mix);
+
+
+// let array=[];
+// array[0]='html';
+// array[1]='js';
+// array[2]='css';
+// console.log(array);
+
+
+// for (let i=0; i<10; i++) {
+// document.write(`<div>text</div>`);
+// }
+
+
+// for (let i=0; i<10; i++) {
+// document.write(`<div>${i} text</div>`)
+// }
+
+// let a=0;
+// while (a<20){
+//     document.write(`<div><h1>text</h1></div>`)
+//     a++
+// }
+// let b=0;
+// while (b<20){
+//     document.write(`<div><h1>${b} text </h1></div>`)
+//     b++
+// }
+
+
+// let numbs=[11, 22, 33, 44, 55, 66, 77, 88, 99, 100]
+// for (let i=0; i < 10; i++){
+//     console.log(numbs[i])
+// }
+// let words=['one','two','three','four','five','six', 'seven', 'eight', 'nine', 'ten']
+// for (let i=0; i<10; i++){
+//     console.log(words[i])
+// }
+
+// let mix=[true, false, 1, 2, 'one', 'two', 'three', 8, 9, 10]
+// for (let i=0; i<10; i++){
+//     console.log(mix[i])
+// }
+
+// let array = [ ];
+// array[0] = 1;
+// array[1] = 'html';
+// array[2] = 2;
+// array[3] = 'css';
+// array[4] = 'js';
+// array[5] = 123;
+// array[6] = 'it';
+// array[7] = true;
+// array[8] = 2222;
+// array[9] = false;
+// for (let i=0;i<10;i++){
+//     console.log(array[i])
+// }
+//
+// for (let i=0;i<10; i++){
+//     console.log('step:'+i+'');
+//     document.write('step:'+i+'');
+// }
+
+// for (let i=0;i<100; i++){
+//     console.log('step'+i+'');
+//     document.write('step'+i+'');
+// }
+//
+//
+// for (let i=0;i<100; i+=2){
+//     console.log('step'+i+'');
+//     document.write('step'+i+'');
+// }
+
+
+// for (let i=0;i<100; i++){
+//     if (i%2===0){
+//     console.log('step'+i+'');
+//     document.write('step'+i+'');
+// }
+// }
+
+
+// for (let i=0;i<100; i++) {
+//     if (i % 2 === 1) {
+//         console.log('step' + i + '');
+//         document.write('step' + i + '');
+//     }
+// }
+
+
+
+
